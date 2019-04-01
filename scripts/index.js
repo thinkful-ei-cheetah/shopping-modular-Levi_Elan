2e1b1269449944bb780070c88c2cfe3ffa5eb615
Elan Green <green.elan5@gmail.com>
2019-04-01 10:54:59 -0500

    add store.js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 'use strict';
 /* global shoppingList, cuid */
+/* global $ */
 
 // eslint-disable-next-line no-unused-vars
 
--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,6 @@
 'use strict';
 /* global store, cuid */
+/* global $ */
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
